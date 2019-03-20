b0caeb6afc9dfe706f6ee8a87ba8bfe79f61ba12
raghava <raghava.billa@alektasolutions.com>
2019-03-20 18:13:31 +0530

    integrated login api and refresh token api

--- src/app/chequeslist/chequeslist.component.ts
+++ src/app/chequeslist/chequeslist.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Request,RequestMethod,Http,Response,Headers,ResponseType, ResponseContentType } from '@angular/http';
+import { CookieService } from 'ngx-cookie';
 
 @Component({
   selector: 'app-chequeslist',
@@ -9,9 +10,16 @@ import { Request,RequestMethod,Http,Response,Headers,ResponseType, ResponseConte
 })
 export class ChequeslistComponent implements OnInit {
 
-  constructor(private router:Router,private http:Http) { }
-
+  constructor(private router:Router,private http:Http,private cookieService:CookieService) { }
+  show: boolean = true;
+logindata:any;
   ngOnInit() {
+
+if(this.cookieService.get("refresh")
+this.router.navigate(['/mobile'])
+
+
+
   }
 login(data){
   console.log(data)
@@ -26,13 +34,16 @@ console.log(json)
   this.http.post("https://3q4jnoy6zf.execute-api.ap-south-1.amazonaws.com/prod/signin",json).subscribe(data => {
   var boy=data.json();
 console.log(boy)
+this.show=false
+this.logindata=boy
 })
 }
 signup(data){
 
+
 var json={
   "operation": "signup",
-  "email": data.value.email,
+  "email": data.value.emUSER_ID_FOR_SRPail,
   "dob": data.value.date,
   "mobile": "+91"+data.value.mobile,
   "name":data.value.name
@@ -43,5 +54,31 @@ console.log(json)
   var boy=data.json();
 console.log(boy)
 })
+}
+changetologin(){
+  this.show=true
+}
+gettoken(data){
+  //console.log("checking "+this.logindata)
+var json={
+
+  "operation": "challenge_mfa",
+  "username": this.logindata.ChallengeParameters.USER_ID_FOR_SRP,
+  "mfa_code": data.value.otp,
+  "session":this.logindata.Session
+
+}
+//console.log(json)
+
+  this.http.post("https://3q4jnoy6zf.execute-api.ap-south-1.amazonaws.com/prod/challenge-mfa",json).subscribe(data => {
+  var boy=data.json();
+console.log(boy)
+this.cookieService.put("access",boy.data.AuthenticationResult.AccessToken)
+this.cookieService.put("refresh",boy.data.AuthenticationResult.RefreshToken)
+
+})
+
+
+
 }
 }
--- src/app/mobile/mobile.component.ts
+++ src/app/mobile/mobile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit,ViewChild ,ElementRef} from '@angular/core';
+import { Request,RequestMethod,Http,Response,Headers,ResponseType, ResponseContentType } from '@angular/http';
+import { CookieService } from 'ngx-cookie';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mobile',
@@ -10,17 +13,36 @@ export class MobileComponent implements OnInit {
   address:any;
   @ViewChild('close') close1: ElementRef;
 
-  constructor() {
+  constructor(private cookieService:CookieService,private http:Http,private router:Router) {
 
     this.address="Locating......"
 }
 add(a){
-
+//
   this.address=a
   console.log(a)
 
 }
   ngOnInit() {
+
+    //this.router.navigate(['/mobile'])
+    var json={
+      "operation": "refresh_token",
+      "refresh_token": this.cookieService.get("refresh")
+
+    }
+    console.log(json)
+
+      this.http.post("https://3q4jnoy6zf.execute-api.ap-south-1.amazonaws.com/prod/refreshtoken",json).subscribe(data => {
+      var boy=data.json();
+    if(boy.Status="Success"){
+      this.cookieService.put("access",boy.data.AuthenticationResult.AccessToken)
+    }
+    else{
+    this.router.navigate(['/'])
+  }
+
+    })
   }
   closemodal(){
 
